0eeca051d26c3824e9d916c9b16c481d239f6820
Erel Vanono <erel2008@gmail.com>
2026-02-16 20:45:21 +0200

    Fix user_service

--- backend/internal/application/user_service.go
+++ backend/internal/application/user_service.go
@@ -2,10 +2,13 @@ package application
 
 import (
 	"backend/internal/domain"
+	domainerrors "backend/internal/domain/errors"
 	"backend/internal/domain/repository"
 	"backend/pkg/contracts"
 	"backend/pkg/errors"
 	"context"
+
+	"github.com/google/uuid"
 )
 
 type UserService struct {
@@ -18,15 +21,18 @@ func NewUserService(userRepo repository.UserRepository) UserService {
 	}
 }
 
-func (s UserService) CreateLocalUser(ctx context.Context, request contracts.CreateLocalUser) *errors.Error {
+func (s UserService) CreateLocalUser(ctx context.Context, request contracts.CreateLocalUser) (domain.UserAggregate, *errors.Error) {
 	var (
 		err  *errors.Error
 		user domain.UserAggregate
 	)
+
+	// Check if user already exists with this email
 	_, err = s.userRepository.GetByEmail(ctx, request.Email)
-	if err != nil {
-		return err
+	if err != nil && err.HTTPStatus() != domainerrors.ErrNotFound.HTTPStatus() {
+		return domain.UserAggregate{}, err
 	}
+
 	userFactory := domain.UserFactory{}
 	user, err = userFactory.Create(
 		nil,
@@ -38,12 +44,12 @@ func (s UserService) CreateLocalUser(ctx context.Context, request contracts.Crea
 	)
 
 	if err != nil {
-		return err
+		return domain.UserAggregate{}, err
 	}
 
 	err = s.userRepository.Create(ctx, user)
 	if err != nil {
-		return err
+		return domain.UserAggregate{}, err
 	}
-	return nil
+	return user, nil
 }
