3fcd3db308e8bf094ce65e1783b0a28610c38f07
Erel Vanono <erel2008@gmail.com>
2026-02-27 12:23:48 +0200

    Add additional intefaces for the application layer

--- backend/internal/application/handlers/unit_of_work.go
+++ backend/internal/application/handlers/unit_of_work.go
@@ -1,11 +1,22 @@
 package handlers
 
 import (
+	"backend/internal/domain/repository"
 	"backend/pkg/errors"
 )
 
-type UnitOfWork interface {
-	Begin() *errors.Error
-	Commit() *errors.Error
-	Rollback() *errors.Error
-}
+type (
+	UnitOfWorkFactory interface {
+		Create() UnitOfWork
+	}
+	UnitOfWork interface {
+		Begin() *errors.Error
+		Commit() *errors.Error
+		Rollback() *errors.Error
+	}
+	RepositoryFactory interface {
+		CreateUserRepository(uow UnitOfWork) repository.UserRepository
+		CreateWorkspaceRepository(uow UnitOfWork) repository.WorkspaceRepository
+	}
+)
+
