8f05f3d0de7163eb5c165999b80c25f7c06d6390
Erel Vanono <erel2008@gmail.com>
2026-03-28 23:16:25 +0300

    fix tests

--- backend/integration_tests/helpers_test.go
+++ backend/integration_tests/helpers_test.go
@@ -38,6 +38,7 @@ type ErrorResponse struct {
 type AuthContext struct {
 	UserID      uuid.UUID
 	UserName    string
+	IsAdmin     bool
 	WorkspaceID uuid.UUID
 }
 
@@ -71,6 +72,7 @@ func addAuth(t *testing.T, req *http.Request, auth AuthContext) {
 	token, err := jwtSvc.GenerateToken(
 		auth.UserID.String(),
 		auth.UserName,
+		auth.IsAdmin,
 		auth.WorkspaceID.String(),
 	)
 	if err != nil {
